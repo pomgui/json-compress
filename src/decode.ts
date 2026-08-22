9aea56780f4cdb472a995269eef03de74f7ead99
wpomier <55899540+pomgui@users.noreply.github.com>
2026-08-22 11:38:26 -0300

    feat: Array shrinking

--- src/decode.ts
+++ src/decode.ts
@@ -1,51 +1,89 @@
-import { RADIX } from "./encode";
+import { RADIX } from './encode';
 
 export function decode(value: any): any {
-    if (!value?.d)
-        return value;
-    const map = value.$;
-    const dateMin = parseInt(value.$$, RADIX);
+  if (!value?.d) return value;
+  const map = value.$;
+  const dateMin = parseInt(value.$$, RADIX);
+  const d = expandAllArrays(value.d);
 
-    return dodecode(value.d);
+  return dodecode(d);
 
-    function dodecode(value: any): any {
-        if (!Array.isArray(value) && typeof value === 'object' && value !== null) {
-            if (value.$ !== undefined) {
-                const keys = Object.keys(value).filter(key => key !== '$');
-                const result = [];
-                keys.forEach(key => value[key] = dodecode(value[key]));
-                const len = value[keys[0]].length;
-                for (let i = 0; i < len; i++) {
-                    const item: any = {};
-                    for (const key of keys) {
-                        item[getKey(key)] = value[key][i];
-                    }
-                    result.push(item);
-                }
-                return result;
-            } else {
-                return Object.entries(value)
-                    .reduce((result, [key, val]) => (result[getKey(key)] = dodecode(val), result), {} as any
-                    );
-            }
-        } else if (Array.isArray(value)) {
-            if (value[0] === '§') {
-                return value.slice(1).map(e => {
-                    if (typeof e == 'number')
-                        return map[e];
-                    if (typeof e == 'string')
-                        return getKey(e);
-                    return dodecode(e);
-                });
-            } else
-                return value.map(dodecode);
-        } else if (typeof value === 'string')
-            value = getKey(value);
-        return value;
-    }
-    function getKey(key: string): string {
-        if (key[0] == '§' && key[1] == '§')
-            return new Date(dateMin + parseInt(key.substring(2), RADIX)).toISOString();
-        return key[0] == '§' ? map[parseInt(key.substring(1), RADIX)] : key;
-    }
+  function dodecode(value: any): any {
+    if (!Array.isArray(value) && typeof value === 'object' && value !== null) {
+      if (value.$ !== undefined) {
+        const keys = Object.keys(value).filter((key) => key !== '$');
+        const result = [];
+        keys.forEach((key) => (value[key] = dodecode(value[key])));
+        const len = value[keys[0]].length;
+        for (let i = 0; i < len; i++) {
+          const item: any = {};
+          for (const key of keys) {
+            item[getKey(key)] = value[key][i];
+          }
+          result.push(item);
+        }
+        return result;
+      } else {
+        return Object.entries(value).reduce(
+          (result, [key, val]) => (
+            (result[getKey(key)] = dodecode(val)),
+            result
+          ),
+          {} as any,
+        );
+      }
+    } else if (Array.isArray(value)) {
+      if (value[0] === '§') {
+        return value.slice(1).map((e) => {
+          if (typeof e == 'number') return map[e];
+          if (typeof e == 'string') return getKey(e);
+          return dodecode(e);
+        });
+      } else return value.map(dodecode);
+    } else if (typeof value === 'string') value = getKey(value);
+    return value;
+  }
+  function getKey(key: string): string {
+    if (key[0] == '§' && key[1] == '§')
+      return new Date(
+        dateMin + parseInt(key.substring(2), RADIX),
+      ).toISOString();
+    return key[0] == '§' ? map[parseInt(key.substring(1), RADIX)] : key;
+  }
 }
+
+const expandAllArrays = (value: any): any => {
+  if (typeof value !== 'object' || !value) return value;
+  if (Array.isArray(value)) {
+    const arr = Array(value.length);
+    for (let i = 0; i < value.length; i++) arr[i] = expandAllArrays(value[i]);
+    if (arr[0] !== 'þ') return arr;
+    else return expandOneArray(arr);
+  } else {
+    const obj = {} as any;
+    for (const key in value) obj[key] = expandAllArrays(value[key]);
+    return obj;
+  }
+};
+
+const expandOneArray = <T = any>(arr: T[]): T[] => {
+  const result: T[] = [];
+  for (let i = 1; i < arr.length; i++) {
+    if (typeof arr[i] !== 'string' || (arr[i] as string)[0] !== 'þ') {
+      result.push(arr[i]);
+    } else {
+      const encoded = (arr[i] as string).substring(1);
+      const g = /^(\d+)([nsb])(.*)$/.exec(encoded);
+      const count = parseInt(g![1]);
+      const value = (
+        g![2] == 'n'
+          ? parseInt(g![3], RADIX) // number
+          : g![2] == 'b'
+            ? g![3] == '1' // true|false
+            : g![3]
+      ) as T;
+      for (let i = 0; i < count; i++) result.push(value);
+    }
+  }
+  return result;
+};
--- src/encode.ts
+++ src/encode.ts
@@ -1,130 +1,198 @@
-
 const ISO_DATE = /^\d{4}-\d{2}-\d{2}/;
 export const RADIX = 36;
 
 export function encode(value: any): any {
-    const pivot = getpivot(value);
-    const valueMap = new Map<string | boolean | null, string>();
-    const dateMap = new Map<string, string | number>();
-    let dateMin: number | string = 0;
+  // First convert the array of objects into object of arrays
+  const pivot = getpivot(value);
+  const valueMap = new Map<string | boolean | null, string>();
+  const dateMap = new Map<string, string | number>();
+  let dateMin: number | string = 0;
 
-    createMap(pivot);
-    const ret = {} as any;
-    if (valueMap.size > 0 || dateMap.size > 0) {
-        if (dateMap.size > 0) ret.$$ = (dateMin as number).toString(RADIX);
-        if (valueMap.size > 0) ret.$ = Array.from(valueMap.keys());
-        ret.d = replaceKeyValue(pivot);
-    } else
-        ret.d = pivot;
-    return ret;
+  createStringMap(pivot);
+  const ret = {} as any;
+  if (valueMap.size > 0 || dateMap.size > 0) {
+    if (dateMap.size > 0) ret.$$ = (dateMin as number).toString(RADIX);
+    if (valueMap.size > 0) ret.$ = Array.from(valueMap.keys());
+    ret.d = replaceKeyValue(pivot);
+  } else ret.d = pivot;
 
-    function createMap(value: any): void {
-        const map = new Map<string | boolean, { type: string, idx: number; num: number }>();
-        const add = (val: string | boolean, isKey: boolean) => {
-            const type = val === null ? 'null' : typeof val;
-            if (!['string', 'boolean', 'null'].includes(type)) return;
-            if (isKey && type == 'string' && /^\d+$/.test(val as string)) return;
-            if (type != 'string' || (val as string).length > 2) {
-                let k = map.get(val);
-                if (!k)
-                    map.set(val, k = { type, idx: map.size, num: 0 });
-                k.num++;
-            }
-        };
+  ret.d = shrinkAllArrays(ret.d);
 
-        JSON.stringify(value, (key, val) => {
-            add(key, true);
-            if (typeof val == 'string' && ISO_DATE.test(val)) {
-                // Is an ISO Date
-                const date = new Date(val);
-                const t = date.getTime();
-                if (!dateMin || (dateMin as number) > t)
-                    dateMin = t;
-                dateMap.set(val, t);
-                return val;
-            }
-            add(val, false);
-            return val;
+  return ret;
+
+  /** Process the object of arrays to populate the maps  */
+  function createStringMap(value: any): void {
+    const map = new Map<
+      string | boolean,
+      { type: string; idx: number; num: number }
+    >();
+
+    const mapAdd = (val: string | boolean, keyval: 'key' | 'value') => {
+      const isKey = keyval === 'key';
+      const type = val === null ? 'null' : typeof val;
+      if (
+        ['string', 'boolean', 'null'].includes(type) &&
+        (isKey || type == 'string' || /^\d+$/.test(val as string)) &&
+        (type != 'string' || (val as string).length > 2)
+      ) {
+        let k = map.get(val);
+        if (!k) map.set(val, (k = { type, idx: map.size, num: 0 }));
+        k.num++;
+      }
+    };
+
+    JSON.stringify(value, (key, val) => {
+      mapAdd(key, 'key');
+      // It's an ISO Date, then convert it into a number and get the min date
+      if (typeof val == 'string' && ISO_DATE.test(val)) {
+        const date = new Date(val);
+        const t = date.getTime();
+        if (!dateMin || (dateMin as number) > t) dateMin = t;
+        dateMap.set(val, t);
+        return val;
+      }
+      mapAdd(val, 'value');
+      return val;
+    });
+
+    // Cleanup the value map
+    Array.from(map.entries())
+      .filter((entry) => entry[1].num > 1)
+      .sort(
+        (a, b) =>
+          b[1].num * String(b[0]).length - a[1].num * String(a[0]).length,
+      )
+      .map(([key, val]) => key)
+      .filter((key, i) => String(key).length > i.toString(RADIX).length + 1)
+      .forEach((key, i) => valueMap.set(key, i.toString(RADIX)));
+
+    // Use the date map, only if there are more than one date
+    if (dateMap.size > 0) {
+      for (const k of dateMap.keys())
+        dateMap.set(
+          k,
+          ((dateMap.get(k)! as number) - (dateMin as number)).toString(RADIX),
+        );
+    }
+  }
+
+  function replaceKeyValue(value: any): any {
+    const type = value === null ? 'null' : typeof value;
+    if (Array.isArray(value)) {
+      const arr = value.map(replaceKeyValue);
+      // Se o array não tem nenhum number
+      if (!arr.some((e) => typeof e == 'number')) {
+        let c = 0;
+        const arrCopy = arr.map((e) => {
+          if (typeof e == 'string' && e[0] == '§' && e[1] != '§') {
+            e = parseInt(e.substring(1), RADIX) as any;
+            c++;
+          }
+          return e;
         });
-
-        // Cleanup the value map
-        Array.from(map.entries())
-            .filter(entry => entry[1].num > 1)
-            .sort((a, b) => b[1].num * String(b[0]).length - a[1].num * String(a[0]).length)
-            .map(([key, val]) => key)
-            .filter((key, i) => String(key).length > i.toString(RADIX).length + 1)
-            .forEach((key, i) => valueMap.set(key, i.toString(RADIX)));
-
-        // Use the date map, only if there are more than one date
-        if (dateMap.size > 0) {
-            for (const k of dateMap.keys())
-                dateMap.set(k, ((dateMap.get(k)! as number) - (dateMin as number)).toString(RADIX));
+        if (c > 1) {
+          arrCopy.unshift('§');
+          return arrCopy;
         }
+      }
+      return arr;
+    } else if (type == 'object' && value !== null) {
+      const newValue: any = {};
+      for (const [key, val] of Object.entries(value)) {
+        const k = valueMap.get(key);
+        const newkey = k !== undefined ? '§' + k : key;
+        newValue[newkey] = replaceKeyValue(val);
+      }
+      return newValue;
     }
-
-    function replaceKeyValue(value: any): any {
-        const type = value === null ? 'null' : typeof value;
-        if (Array.isArray(value)) {
-            const arr = value.map(replaceKeyValue);
-            if (!arr.some(e => typeof e == 'number')) {
-                let c = 0;
-                const arrCopy = arr.map(e => {
-                    if (typeof e == 'string' && e[0] == '§' && e[1] != '§') {
-                        e = parseInt(e.substring(1), RADIX) as any;
-                        c++;
-                    }
-                    return e;
-                })
-                if (c > 1) {
-                    arrCopy.unshift('§');
-                    return arrCopy;
-                }
-            }
-            return arr;
-        } else if (type == 'object' && value !== null) {
-            const newValue: any = {};
-            for (const [key, val] of Object.entries(value)) {
-                const k = valueMap.get(key);
-                const newkey = k !== undefined ? '§' + k : key;
-                newValue[newkey] = replaceKeyValue(val);
-            }
-            return newValue;
-        }
-        if (['string', 'boolean', 'null'].includes(type)) {
-            const k = valueMap.get(value);
-            if (k !== undefined)
-                return '§' + k;
-        }
-        if (type === 'string') {
-            const d = dateMap.get(value);
-            if (d !== undefined)
-                return '§§' + d;
-        }
-        return value;
+    if (['string', 'boolean', 'null'].includes(type)) {
+      const k = valueMap.get(value);
+      if (k !== undefined) return '§' + k;
     }
-
-    function getpivot(value: any): any {
-        if (Array.isArray(value)) {
-            if (value.length > 1 &&
-                typeof value[0] === 'object' &&
-                value[0] !== null &&
-                !Array.isArray(value[0])
-            ) {
-                return (Object.keys(value[0]) as any[])
-                    .reduce((result, key) =>
-                        (result[key] = value.map(item => getpivot(item[key])), result),
-                        { $: 0 } as any as Record<string, any[]>
-                    );
-            } else
-                return value.map(getpivot) as any;
-        }
-        else if (value instanceof Date) {
-            return value.toISOString() as any;
-        } else if (typeof value === 'object' && value !== null) {
-            return Object.entries(value)
-                .reduce((result, [key, val]) => (result[key] = getpivot(val), result), {} as any
-                );
-        }
-        return value;
+    if (type === 'string') {
+      const d = dateMap.get(value);
+      if (d !== undefined) return '§§' + d;
     }
+    return value;
+  }
 }
+
+const getpivot = (value: any): any => {
+  if (Array.isArray(value)) {
+    if (
+      value.length > 1 &&
+      typeof value[0] === 'object' &&
+      value[0] !== null &&
+      !Array.isArray(value[0])
+    ) {
+      const result: Record<string, any[]> = { $: 0 as any };
+      for (const key of Object.keys(value[0]))
+        result[key] = value.map((item) => getpivot(item[key]));
+      return result;
+    } else return value.map(getpivot) as any;
+  } else if (value instanceof Date) {
+    return value.toISOString() as any;
+  } else if (typeof value === 'object' && value !== null) {
+    return Object.entries(value).reduce(
+      (result, [key, val]) => ((result[key] = getpivot(val)), result),
+      {} as any,
+    );
+  }
+  return value;
+};
+
+const shrinkOneArray = <T extends string | boolean | number>(arr: T[]): T[] => {
+  const result: T[] = [];
+  let i = 0;
+  const n = arr.length;
+  let used = false;
+
+  while (i < n) {
+    const curr: T | string = arr[i];
+    const currType = typeof curr;
+    // Only consider strings (or numbers)
+    if (currType == 'number' || currType == 'string' || currType == 'boolean') {
+      let count = 1;
+      while (i + 1 < n && arr[i + 1] === curr) {
+        count++;
+        i++;
+      }
+      // Only consider a minumum o elements than surpasses the compressed prefix size
+      // otherwise it does not worth it
+      const currLen = String(curr).length;
+      const type = currType[0];
+      if (
+        count * (currLen + (type == 's' ? 3 : 1)) >=
+        9 + (type == 'b' ? 1 : currLen)
+      ) {
+        let newval: any = curr;
+        if (type == 'n') newval = curr.toString(RADIX);
+        else if (type == 'b') newval = curr ? '1' : '0';
+        const topush = `þ${count}${type}${newval}`;
+        result.push(topush as T);
+        used = true;
+      } else {
+        for (let i = 0; i < count; i++) result.push(curr);
+      }
+    } else result.push(curr);
+    i++;
+  }
+  if (used) result.unshift('þ' as T);
+
+  return result;
+};
+
+/** Search recursively the structure and compress every found array */
+const shrinkAllArrays = (value: any): any => {
+  if (typeof value !== 'object' || !value) return value;
+  if (Array.isArray(value)) {
+    const arr = value.map(shrinkAllArrays);
+    return shrinkOneArray(arr);
+  } else {
+    const obj = {} as any;
+    for (const key in value) {
+      obj[key] = shrinkAllArrays(value[key]);
+    }
+    return obj;
+  }
+};
